2dfddbc1e1a71048ed0214a3fa3fcde3b1232465
Puusa Jere TTK17SP <jerepuusa@kamk.fi>
2017-09-20 17:59:27 +0300

    Did demo project

---
+++ Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    float horizontal, vertical;
+
+    Vector3 movement;
+
+    public float speed = 5;
+    
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        movement = new Vector3(horizontal, 0, vertical);
+
+        Vector3 targetMovement = movement * speed * Time.deltaTime;
+
+        rb.MovePosition(transform.position + targetMovement);
+    }
+
+}
